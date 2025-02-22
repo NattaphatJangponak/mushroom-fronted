907a221d45e90fb907a14ef53099c7798bbfb98d
Nattaphat Jangponak <81207175+NattaphatJangponak@users.noreply.github.com>
2025-02-23 00:02:36 +0700

    commit

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -15,19 +15,23 @@ import Growing from "../pages/Growing";
 import ViewCultivation from "../pages/ViewCultivation";
 // router
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/homepage" element={<HomePage />} />
-            <Route path="/systemOverView" element={<SystemOverView />} />
-            <Route path="/application" element={<Application />} />
-            <Route path="/rowselection" element={<RowSelectionPage/>}/> 
-            <Route path="/potselection" element={<PotSelectionPage />} />
-            <Route path="/masterdata" element={<Materdata />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/homepage" element={<HomePage />} />
+      <Route path="/systemOverView" element={<SystemOverView />} />
+      <Route path="/application" element={<Application />} />
+      <Route path="/rowselection" element={<RowSelectionPage />} />
+      <Route path="/potselection" element={<PotSelectionPage />} />
+      <Route path="/masterdata" element={<Materdata />} />
+      <Route path="/mushroom-cultivation" element={<Cultivation />} />
+      <Route path="/mushroom-cultivation/view/" element={<ViewCultivation />} />
+      <Route path="/mushroom-growing" element={<Growing />} />
+
+    </Routes>
+  );
 };
 
 export default AppRouter;
